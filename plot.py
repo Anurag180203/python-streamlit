7e50e9d455696d702b6c396e97aa56e239bbf28e
Anurag180203 <anurag180203@gmail.com>
2023-06-05 15:29:56 +0530

    working on plots using plotly and altair

---
+++ plot.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+import altair as alt
+
+import plotly.express as px
+import plotly.figure_factory as ff
+
+st.header('1. Altair Scatter Plot')
+chart_data = pd.DataFrame(np.random.randn(500,5),columns=['a','b','c','d','e'])
+chart = alt.Chart(chart_data).mark_circle().encode(x='a',y='b',size='c',tooltip=['a','b','c','d','e'])
+st.altair_chart(chart)
+
+st.header('2. Interactive charts')
+st.subheader('2.1. Line Chart')
+
+df = pd.read_csv("lang_data.csv")
+lang_list = df.columns.tolist()
+lang_choices = st.multiselect("Choose your languages",lang_list)
+df = df[lang_choices]
+st.line_chart(df)
+
+st.subheader('2.2. Area Chart')
+st.area_chart(df)
+
+st.header('3. Data visualisation with Plotly')
+
+st.subheader('3.1 Displaying the dataset')
+df = pd.read_csv("tips.csv")
+st.dataframe(df.head())
+
+st.subheader('3.2 Pie Chart')
+fig = px.pie(df,values = 'total_bill',names='day',opacity=.8)
+st.plotly_chart(fig)
+fig = px.pie(df,values = 'total_bill',names='time')
+st.plotly_chart(fig)
+fig = px.pie(df,values = 'total_bill',names='size')
+st.plotly_chart(fig)
+
+st.subheader('3.3 Pie Chart with multiple parameters')
+fig = px.pie(df,values = 'total_bill',names='day',opacity=.7,color_discrete_sequence=px.colors.sequential.RdBu)
+st.plotly_chart(fig)
+
+st.subheader('3.4 Histogram')
+x1 = np.random.randn(200)
+x2 = np.random.randn(200)
+x3 = np.random.randn(200)
+
+hist_data = [x1,x2,x3]
+group_labels = ['Group-1','Group-2','Group-3']
+fig = ff.create_distplot(hist_data,group_labels,bin_size=[.1,.25,.5])
+
+st.plotly_chart(fig)
